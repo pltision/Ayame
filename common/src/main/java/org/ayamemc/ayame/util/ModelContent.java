49a3f9a600fe743a1a58ea692bc7db03dd2f012e
HappyRespawnanchor <80967824+HappyRespawnanchor@users.noreply.github.com>
2024-11-09 22:09:40 +0800

    ModelContent

--- common/src/main/java/org/ayamemc/ayame/util/ModelContent.java
+++ common/src/main/java/org/ayamemc/ayame/util/ModelContent.java
@@ -1,65 +1,49 @@
-/*
- *     Custom player model mod. Powered by GeckoLib.
- *     Copyright (C) 2024  CrystalNeko, HappyRespawnanchor, pertaz(Icon Designer)
- *
- *     This file is part of Ayame.
- *
- *     Ayame is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     Ayame is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with Ayame.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package org.ayamemc.ayame.util;
+
 import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PathPackResources;
 import net.minecraft.server.packs.linkfs.LinkFileSystem;
-import net.minecraft.server.packs.repository.FolderRepositorySource;
-import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackDetector;
 import net.minecraft.world.level.validation.DirectoryValidator;
 import org.ayamemc.ayame.Ayame;
 import org.ayamemc.ayame.model.resource.ModelResourceRegistry;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.Stream;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.io.IOException;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
-import static org.ayamemc.ayame.Ayame.LOGGER;
 
 public class ModelContent extends PackDetector<ModelResourceRegistry.ModelFile> {
+
     public ModelContent(DirectoryValidator validator) {
         super(validator);
     }
 
     @Nullable
+    @Override
     protected ModelResourceRegistry.ModelFile createZipPack(Path path) {
-        FileSystem fileSystem = path.getFileSystem();
-        if (fileSystem != FileSystems.getDefault() && !(fileSystem instanceof LinkFileSystem)) {
-            Ayame.LOGGER.info("Can't open pack archive at {}", path);
-            return null;
-        } else {
-            return new FilePackResources.FileResourcesSupplier(path);
+        try (ZipFile zipFile = new ZipFile(path.toFile())) {
+            ZipEntry entry = zipFile.getEntry("index.json");
+            if (entry != null) {
+                return new ModelResourceRegistry.ModelFile();
+            }
+        } catch (IOException e) {
+            Ayame.LOGGER.error("Failed to read ZIP pack at path: {}", path, e);
         }
+        return null;
     }
 
+    @Override
     protected ModelResourceRegistry.ModelFile createDirectoryPack(Path path) {
-        return new PathPackResources.PathResourcesSupplier(path);
+        Path indexFilePath = path.resolve("index.json");
+        if (Files.exists(indexFilePath)) {
+            return new ModelResourceRegistry.ModelFile();
+        }
+        return null;
     }
-
-
 }
